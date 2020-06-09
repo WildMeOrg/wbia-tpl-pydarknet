941435f0fa927ed4bc445e673f82dee4c3e69301
Michael Mulich <michael.mulich@gmail.com>
2020-06-09 15:30:41 -0700

    Simplify the setup script

--- setup.py
+++ setup.py
@@ -1,50 +1,52 @@
 #!/usr/bin/env python
-from __future__ import absolute_import, division, print_function
-from utool import util_cplat
-from setuptools import setup
-from utool import util_setup
+import ast
+import pathlib
 
-#delete_on_clean = ['libpydarknet.dll', 'libpydarknet.dll.a', 'libpydarknet.so', 'libpydarknet.dylib', 'build/']
-clutter_patterns = [
-    'libpydarknet.dll',
-    'libpydarknet.dll.a',
-    'libpydarknet.so',
-    'libpydarknet.dylib',
-    'pydarknet.egg-info',
-    'build/',
-    '*.pyo',
-    '*.pyc'
-]
+from setuptools import setup, find_packages
 
 
-def build_command():
-    """ Build command run by utool.util_setup """
-    if util_cplat.WIN32:
-        raise NotImplementedError('Canno build this module on Windows, because it\'s the worst')
-    else:
-        util_cplat.shell('./unix_build.sh')
+setup_requires = ()
+install_requires = ()
 
 
-INSTALL_REQUIRES = [
-    #'detecttools >= 1.0.0.dev1',
-]
+def parse_version(fpath):
+    """Statically parse the version number from a python file"""
+    py_module = pathlib.Path(fpath)
+    if not py_module.exists():
+        raise ValueError('fpath={!r} does not exist'.format(fpath))
+    with py_module.open('r') as fb:
+        sourcecode = fb.read()
+    pt = ast.parse(sourcecode)
+
+    class VersionVisitor(ast.NodeVisitor):
+        def visit_Assign(self, node):
+            for target in node.targets:
+                if getattr(target, 'id', None) == '__version__':
+                    self.version = node.value.s
+
+    visitor = VersionVisitor()
+    visitor.visit(pt)
+    return visitor.version
+
+
+name = 'wbia-pydarknet'
+version = parse_version('pydarknet/__init__.py')  # must be global for git tags
+
+setup_kwargs = dict(
+    name=name,
+    version=version,
+    license='YOLO',
+    description=('Detects objects in images using Darknet'),
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    url='https://github.com/WildbookOrg/wbia-tpl-pydarknet',
+    author='Jason Parham, WildMe Developers',
+    author_email='dev@wildme.org',
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    # package_data={'build': util_cplat.get_dynamic_lib_globstrs()},
+)
+
 
 if __name__ == '__main__':
-    kwargs = util_setup.setuptools_setup(
-        name='pydarknet',
-        packages=['pydarknet', 'build'],
-        #packages=util_setup.find_packages(),
-        version=util_setup.parse_package_for_version('pydarknet'),
-        licence=util_setup.read_license('LICENSE'),
-        long_description=util_setup.parse_readme('README.md'),
-        description=('Detects objects in images using Darknet'),
-        url='https://github.com/bluemellophone/pydarknet',
-        author='Jason Parham',
-        author_email='bluemellophone@gmail.com',
-        clutter_patterns=clutter_patterns,
-        install_requires=INSTALL_REQUIRES,
-        package_data={'build': util_cplat.get_dynamic_lib_globstrs()},
-        build_command=build_command,
-        setup_fpath=__file__,
-    )
-    setup(**kwargs)
+    import skbuild
+    skbuild.setup(**setup_kwargs)
