6cdfdd53875705689ee5e8948013408e4fe55ca1
karen chan <karen@karen-chan.com>
2020-07-01 21:48:18 +0100

    Run brunette on all files

--- _graveyard/data/labels/make_labels.py
+++ _graveyard/data/labels/make_labels.py
@@ -111,6 +111,6 @@ labels = [
 
 for word in labels:
     os.system(
-        "convert -fill black -background white -bordercolor white -border 4 -font futura-normal -pointsize 18 label:\"%s\" \"%s.png\""
+        'convert -fill black -background white -bordercolor white -border 4 -font futura-normal -pointsize 18 label:"%s" "%s.png"'
         % (word, word)
     )
